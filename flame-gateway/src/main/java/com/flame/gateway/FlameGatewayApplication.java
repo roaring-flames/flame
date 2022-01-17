601742e354502b8482bcf4b18911e1e7d1525687
jinliansheng <jinliansheng@163.com>
2022-01-17 10:58:17 +0800

    添加网关服务

---
+++ flame-gateway/src/main/java/com/flame/gateway/FlameGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.flame.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlameGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlameGatewayApplication.class, args);
+    }
+
+}
---
+++ flame-gateway/src/test/java/com/flame/gateway/FlameGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flame.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlameGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
