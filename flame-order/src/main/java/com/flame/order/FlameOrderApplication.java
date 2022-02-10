ab818bf5570f7330b5e902b4c5e993476661a748
jinliansheng <jinliansheng@163.com>
2022-02-10 17:08:51 +0800

    集成分布式事务seata

--- flame-order/src/main/java/com/flame/order/FlameOrderApplication.java
+++ flame-order/src/main/java/com/flame/order/FlameOrderApplication.java
@@ -2,11 +2,13 @@ package com.flame.order;
 
 import com.flame.common.config.CorsConfig;
 import com.flame.common.swagger.Knife4jConfig;
+import io.seata.spring.annotation.datasource.EnableAutoDataSourceProxy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Import;
 
+@EnableAutoDataSourceProxy //开启代理数据源
 @EnableDiscoveryClient
 @SpringBootApplication
 @Import({Knife4jConfig.class,CorsConfig.class})
